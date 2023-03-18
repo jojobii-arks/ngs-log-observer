f3cc0da11da0b29af7c8939b72ab2adf63acda54
joseph-bravo <josephangelobravo@gmail.com>
2023-03-17 23:13:16 -0700

    init: basic implementation complete

--- src/preload/api.ts
+++ src/preload/api.ts
@@ -7,6 +7,8 @@ const api = {
     ipcRenderer.on(CHANNELS.ACTION_NEW, callback),
   onAlert: (callback: Parameters<typeof ipcRenderer.on>[1]) =>
     ipcRenderer.on(CHANNELS.ALERT, callback),
+  onNewAction: (callback: Parameters<typeof ipcRenderer.on>[1]) =>
+    ipcRenderer.on(CHANNELS.ACTION_NEW, callback),
   getGameDirectory: async () => {
     return await ipcRenderer.invoke(CHANNELS.GET_GAME_DIR);
   },
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { ActionLogItem } from 'src/lib/types';
 import useSWR from 'swr';
 
 export default function App(): JSX.Element {
@@ -6,11 +7,19 @@ export default function App(): JSX.Element {
     'gameDirectory',
     async () => await api.getGameDirectory()
   );
+  const [log, setLog] = useState<ActionLogItem[]>([]);
 
   useEffect(() => {
+    api.clear();
+
     /** Initialize API Listeners */
     api.onAlert((_, value) => {
       alert(value);
+    });
+
+    api.onNewAction((_, value) => {
+      console.log(value);
+      setLog([...log, value]);
     });
 
     /** Remove listeners on unmount */
@@ -19,13 +28,28 @@ export default function App(): JSX.Element {
     };
   });
   return (
-    <main className="p-8">
-      <header className="rounded-xl bg-base-300 p-4">
+    <main className="flex h-[100vh] flex-col">
+      <header className="rounded-b-xl bg-base-300 p-4">
         <h1 className="mb-2 text-4xl font-black">NGS Log Observer</h1>
         <p>Using logs from {gameDirectory}</p>
       </header>
-      <div>
-        <ul></ul>
+      <div className="flex-auto overflow-y-scroll">
+        <table className="table-compact table w-full">
+          <thead>
+            <th>Type</th>
+            <th>Data</th>
+            <th>Time</th>
+          </thead>
+          <tbody>
+            {[...log].reverse().map((action: ActionLogItem) => (
+              <tr key={action.action_id + action.log_time}>
+                <td>{action.item_name ? `📥` : `💰`}</td>
+                <td>{action.item_name ? action.item_name : action.item_num}</td>
+                <td>{new Date(action.log_time).toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </main>
   );
