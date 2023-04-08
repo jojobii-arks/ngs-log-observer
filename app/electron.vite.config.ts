7fad33c961e9b53e567ed82008f2410787fa5cae
jojobii <jojobii.arks@gmail.com>
2023-04-08 12:20:29 -0700

    init: svelte

--- app/electron.vite.config.ts
+++ app/electron.vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
-import react from '@vitejs/plugin-react';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   main: {
@@ -12,9 +12,9 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src'),
+        '@renderer': resolve('src/renderer/app'),
       },
     },
-    plugins: [react()],
+    plugins: [svelte()],
   },
 });
---
+++ app/src/renderer/app/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    api.onAlert((_, value) => {
+      alert(value);
+    });
+  });
+</script>
