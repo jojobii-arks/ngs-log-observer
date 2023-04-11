71bd210e94762875ffd8faf8afdde35d1f33f445
jojobii <jojobii.arks@gmail.com>
2023-04-11 12:25:10 -0700

    feat(app): add window shadow

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -3,8 +3,16 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+use window_shadows::set_shadow;
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            set_shadow(&window, true).unwrap();
+            Ok(())
+        })
         .plugin(tauri_plugin_fs_watch::init())
         .plugin(tauri_plugin_fs_extra::init())
         // rust-ignore
